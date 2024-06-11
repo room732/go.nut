53c7d8ab9c7c928bf365d7e8431c489b488e2984
maxxant <maxxant@gmail.com>
2024-06-11 13:23:13 +0300

    example dir

---
+++ example/example.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	nut "github.com/robbiet480/go.nut"
+)
+
+func main() {
+	for {
+		ExampleGetUPSList()
+		fmt.Println()
+		time.Sleep(time.Second * 3)
+	}
+}
+
+// This example connects to NUT, authenticates and returns the first UPS listed.
+func ExampleGetUPSList() {
+	client, connectErr := nut.Connect("127.0.0.1")
+	if connectErr != nil {
+		fmt.Print(connectErr)
+	}
+	_, authenticationError := client.Authenticate("username", "password")
+	if authenticationError != nil {
+		fmt.Print(authenticationError)
+	}
+	upsList, listErr := client.GetUPSList()
+	if listErr != nil {
+		fmt.Print(listErr)
+	}
+	fmt.Print("First UPS", upsList[0])
+}
